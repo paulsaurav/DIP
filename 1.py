7fb6551703a66a91c162f5989058bd241b655148
SAURAV PAUL <72635245+paulsaurav@users.noreply.github.com>
2022-08-19 08:57:27 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+img1 = mpimg.imread('Tanjiro.png')
+imgplot = plt.imshow(img1)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
